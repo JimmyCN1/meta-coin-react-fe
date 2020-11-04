8767b4010dcf999b3c344337b791fd3c36fbd9b7
JimmyCN1 <jimmy.c.njoku@gmail.com>
2020-11-04 22:50:16 +1100

    make request to ganache

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Web3 from 'web3'
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [state, setState] = useState({
+    account: ""
+  });
+
+  useEffect(() => {
+    async function loadBlockchainData() {
+      const web3 = new Web3(Web3.givenProvider || "http://localhost:7545")
+      const accounts = await web3.eth.getAccounts()
+    
+      setState({...state, account: accounts[0]})
+    }
+
+    loadBlockchainData()
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>{state.account}</h2>
       </header>
     </div>
   );
